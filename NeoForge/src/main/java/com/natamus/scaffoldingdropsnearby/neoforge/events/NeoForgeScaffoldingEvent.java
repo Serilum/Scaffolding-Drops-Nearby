7ef9b25c636a90ef3ecc8b277126647576c677c4
Rick South <rick@natamus.com>
2024-05-01 16:31:31 +0200

    Scaffolding Drops Nearby source code for 1.20.6.

---
+++ NeoForge/src/main/java/com/natamus/scaffoldingdropsnearby/neoforge/events/NeoForgeScaffoldingEvent.java
@@ -0,0 +1,27 @@
+package com.natamus.scaffoldingdropsnearby.neoforge.events;
+
+import com.natamus.collective.functions.WorldFunctions;
+import com.natamus.scaffoldingdropsnearby.events.ScaffoldingEvent;
+import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.neoforge.event.level.BlockEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+
+@EventBusSubscriber
+public class NeoForgeScaffoldingEvent {
+	@SubscribeEvent
+	public static void onScaffoldingItem(EntityJoinLevelEvent e) {
+		ScaffoldingEvent.onScaffoldingItem(e.getLevel(), e.getEntity());
+	}
+	
+	@SubscribeEvent
+	public static void onBlockBreak(BlockEvent.BreakEvent e) {
+		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
+		if (level == null) {
+			return;
+		}
+
+		ScaffoldingEvent.onBlockBreak(level, e.getPlayer(), e.getPos(), e.getState(), null);
+	}
+}
