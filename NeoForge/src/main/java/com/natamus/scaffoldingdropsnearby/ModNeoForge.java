e5ea69723364ab3bafa612a32ce30e0b387eacc2
Rick South <rick@natamus.com>
2024-06-14 05:13:14 +0200

    Scaffolding Drops Nearby source code for 1.21.0.

---
+++ NeoForge/src/main/java/com/natamus/scaffoldingdropsnearby/ModNeoForge.java
@@ -0,0 +1,30 @@
+package com.natamus.scaffoldingdropsnearby;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.scaffoldingdropsnearby.neoforge.events.NeoForgeScaffoldingEvent;
+import com.natamus.scaffoldingdropsnearby.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge(IEventBus modEventBus) {
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeScaffoldingEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
