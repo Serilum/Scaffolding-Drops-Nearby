8e46aa90a64f59c3ff501b15cf16f18ad54503a1
Rick South <rick@natamus.com>
2026-05-01 23:47:52 +0200

    (v3.5) (26.1.2 NeoForge) Updated to work with NeoForge version 26.1.2.21-beta and above.

--- NeoForge/src/main/java/com/natamus/scaffoldingdropsnearby/neoforge/events/NeoForgeScaffoldingEvent.java
+++ NeoForge/src/main/java/com/natamus/scaffoldingdropsnearby/neoforge/events/NeoForgeScaffoldingEvent.java
@@ -3,9 +3,9 @@ package com.natamus.scaffoldingdropsnearby.neoforge.events;
 import com.natamus.collective.functions.WorldFunctions;
 import com.natamus.scaffoldingdropsnearby.events.ScaffoldingEvent;
 import net.minecraft.world.level.Level;
-import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
-import net.neoforged.neoforge.event.level.BlockEvent;
 import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
+import net.neoforged.neoforge.event.level.block.BreakBlockEvent;
 
 public class NeoForgeScaffoldingEvent {
 	@SubscribeEvent
@@ -14,7 +14,7 @@ public class NeoForgeScaffoldingEvent {
 	}
 	
 	@SubscribeEvent
-	public static void onBlockBreak(BlockEvent.BreakEvent e) {
+	public static void onBlockBreak(BreakBlockEvent e) {
 		Level level = WorldFunctions.getWorldIfInstanceOfAndNotRemote(e.getLevel());
 		if (level == null) {
 			return;
